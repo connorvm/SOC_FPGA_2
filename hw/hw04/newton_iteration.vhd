a65d11acf501f28cedf951c11732b5fa5ad3b340
Connor Van Meter <akdreamin21@gmail.com>
2020-02-04 17:17:07 -0700

    Edited newton_iteration code. Changed y_current to an input and added x as an input.

--- hw/hw04/newton_iteration.vhd
+++ hw/hw04/newton_iteration.vhd
@@ -10,20 +10,21 @@ entity newton_iteration is
   generic (W_bits   : positive := 8;
            F_bits   : positive := 4);
 
-  port (input_vector  : in  std_logic_vector (7 downto 0);
-	output_vector : out std_logic_vector (7 downto 0));
+  port (y_current : in  unsigned(W_bits - 1 downto 0);
+	x	  : in  unsigned(W_bits - 1 downto 0);
+	output    : out unsigned(W_bits - 1 downto 0));
 end entity;
 
 architecture newton_iteration_arch of newton_iteration is
 
-  signal input     : unsigned(7 downto 0);
-  signal output    : unsigned(7 downto 0);
+  --signal input     : unsigned(7 downto 0);
+  --signal output    : unsigned(7 downto 0);
   signal y_o	   : unsigned(W_bits - 1 downto 0);
-  signal x  	   : unsigned(W_bits - 1 downto 0);
-  signal y_next	   : unsigned(2*W_bits - 1 downto 0);
-  signal y_current : unsigned(W_bits - 1 downto 0);
+  --signal x  	   : unsigned(W_bits - 1 downto 0);
+  signal y_next	   : unsigned(4*W_bits - 1 downto 0);
+  --signal y_current : unsigned(W_bits - 1 downto 0);
   signal temp	   : unsigned(2*W_bits - 1 downto 0);
-  signal top	   : unsigned(2*W_bits - 1 downto 0);
+  signal top	   : unsigned(4*W_bits - 1 downto 0);
 
   begin
 
@@ -35,7 +36,7 @@ architecture newton_iteration_arch of newton_iteration is
     temp <= y_current * y_current;
     top  <= y_current * (3 - (x * temp));
     y_next <= top / 2;
-    y_current <= y_next(W_bits - 1 downto 0);
+    output <= y_next(W_bits - 1 downto 0);
 
    end process;
 
