70605a292c9ea2e3f7d300a6e1870494fa1fe398
Connor Van Meter <akdreamin21@gmail.com>
2020-02-11 13:28:03 -0700

    Edited comments

--- hw/hw04/newton_iteration.vhd
+++ hw/hw04/newton_iteration.vhd
@@ -1,5 +1,6 @@
 -- Authors: Connor Van Meter & Alex Salois
 -- EELE 468
+-- HW_4
 
 library IEEE;
 use IEEE.numeric_std.all;
@@ -17,12 +18,8 @@ end entity;
 
 architecture newton_iteration_arch of newton_iteration is
 
-  --signal input     : unsigned(7 downto 0);
-  --signal output    : unsigned(7 downto 0);
   signal y_o	   : unsigned(W_bits - 1 downto 0);
-  --signal x  	   : unsigned(W_bits - 1 downto 0);
   signal y_next	   : unsigned(4*W_bits - 1 downto 0);
-  --signal y_current : unsigned(W_bits - 1 downto 0);
   signal temp	   : unsigned(2*W_bits - 1 downto 0);
   signal top	   : unsigned(4*W_bits - 1 downto 0);
 
@@ -42,8 +39,5 @@ architecture newton_iteration_arch of newton_iteration is
 
    end process;
 
---    input  <= unsigned(input_vector);
---    output <= input + "00000001";
---    output_vector <= std_logic_vector(output);
 
 end architecture;
