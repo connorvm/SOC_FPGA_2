9652bdcea1bf1c1afba9ade69a833a756b5bab18
Alex Salois <asalois.scholar@gmail.com>
2020-02-26 15:52:24 -0700

    tried to find raddix

--- hw/hw04/newton_iteration.vhd
+++ hw/hw04/newton_iteration.vhd
@@ -21,14 +21,19 @@ end entity;
 architecture newton_iteration_arch of newton_iteration is
 
   signal y_relay_0 : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal y_relay_1 : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal y_relay_2 : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal y_relay_3 : unsigned(W_bits - 1 downto 0) := (others => '0');
   signal x_relay_0 : unsigned(W_bits - 1 downto 0) := (others => '0');
-  signal out_0   : unsigned(W_bits - 1 downto 0) := (others => '0');
-  signal out_1   : unsigned(W_bits - 1 downto 0) := (others => '0');
-  signal out_2   : unsigned(W_bits - 1 downto 0) := (others => '0');
-  signal out_3   : unsigned(W_bits - 1 downto 0) := (others => '0');
-  signal Y_next  : unsigned(4*W_bits - 1 downto 0) := (others => '0');
-  signal square  : unsigned(2*W_bits - 1 downto 0) := (others => '0');
-  signal top	 : unsigned(4*W_bits - 1 downto 0) := (others => '0');
+  signal out_0     : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal out_1     : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal out_2     : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal out_3     : unsigned(W_bits - 1 downto 0) := (others => '0');
+  signal Y_next    : unsigned(4*W_bits - 1 downto 0) := (others => '0');
+  signal xy_square : unsigned(3*W_bits - 1 downto 0) := (others => '0');
+  signal minus     : unsigned(3*W_bits - 1 downto 0) := (others => '0');
+  signal square    : unsigned(2*W_bits - 1 downto 0) := (others => '0');
+  signal top	   : unsigned(4*W_bits - 1 downto 0) := (others => '0');
 
   begin
     --y_(n+1) = (y_n(3 -x(y_n)^2))/2
@@ -37,14 +42,16 @@ architecture newton_iteration_arch of newton_iteration is
     if(reset = '0') then
 	out_n <= "0";
     elsif(rising_edge(clock)) then
-    	square 	<= y_in * y_in; -- square y_current
-    	top  	<= y_relay_0 * (3 - (x_relay_0 * square));
-     	y_next 	<= shift_right(top, 1); -- divide by 2
-	out_0 	<= y_next(W_bits  -1 downto 0);
-	out_1 	<= y_next(2*W_bits - F_bits -1 downto W_bits - F_bits);
-	out_2 	<= y_next(3*W_bits - F_bits -1 downto 2*W_bits - F_bits);
-	out_3 	<= y_next(4*W_bits - F_bits -1 downto 3*W_bits - F_bits);
-    	out_n 	<= y_next(2*W_bits + F_bits -1 downto 1*W_bits + F_bits);
+    	square 	  <= y_in * y_in; -- square y_current
+    	xy_square <= x_relay_0 * square;
+	minus 	  <= 3 - xy_square;
+	top       <= y_relay_3 * minus;
+     	y_next 	  <= shift_right(top, 1); -- divide by 2
+	out_0 	  <= y_next((4*F_bits -1) + (W_bits - F_bits) -1 downto 4*F_bits - (W_bits - F_bits) -1);
+	out_1 	  <= y_next((4*F_bits -1) + (W_bits - F_bits) +1 downto 4*F_bits - (W_bits - F_bits) +1);
+	--out_2 	<= y_next((4*F_bits -1) + (W_bits - F_bits) -1 downto 4*F_bits - (W_bits - F_bits));
+	--out_3 	<= y_next((4*F_bits -1) + (W_bits - F_bits) -1 downto 4*F_bits - (W_bits - F_bits));
+    	out_n 	<= y_next((4*F_bits -1) + (W_bits - F_bits) downto 4*F_bits - (W_bits - F_bits));
 
     end if;
   end process;
@@ -55,6 +62,9 @@ architecture newton_iteration_arch of newton_iteration is
 	--out_n <= "0";
     elsif(rising_edge(clock)) then
     	y_relay_0 <= y_in;
+	y_relay_1 <= y_relay_0;
+	y_relay_2 <= y_relay_1;
+	y_relay_3 <= y_relay_2;
 	x_relay_0 <= x;
 
     end if;
