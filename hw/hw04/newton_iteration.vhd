23da69c63df82b37a060936aebf363907864740f
Alexander Salois <asalois.scholar@gmail.com>
2020-02-27 12:23:02 -0700

    fixed formatting

--- hw/hw04/newton_iteration.vhd
+++ hw/hw04/newton_iteration.vhd
@@ -8,14 +8,14 @@ use IEEE.std_logic_1164.all;
 
 entity newton_iteration is
 
-  generic (W_bits   : positive; 
+  generic (W_bits   : positive;
            F_bits   : positive);
 
-  port (clock         : in std_logic;
-  	reset         : in std_logic;
-	y_in          : in  unsigned(W_bits - 1 downto 0);	 
- 	x	      : in  unsigned(W_bits - 1 downto 0);
-	out_n         : out unsigned(W_bits - 1 downto 0));
+  port (clock   : in std_logic;
+  	    reset   : in std_logic;
+	      y_in    : in  unsigned(W_bits - 1 downto 0);
+ 	      x	      : in  unsigned(W_bits - 1 downto 0);
+	      out_n   : out unsigned(W_bits - 1 downto 0));
 end entity;
 
 architecture newton_iteration_arch of newton_iteration is
@@ -40,30 +40,30 @@ architecture newton_iteration_arch of newton_iteration is
   begin
     --y_(n+1) = (y_n(3 -x(y_n)^2))/2
   process (clock, reset)
-   begin
+  begin
     if(reset = '0') then
-	out_n <= "0";     		
+	out_n <= "0";
     elsif(rising_edge(clock)) then
     	square 	  <= y_in * y_in; -- square y_current
     	xy_square <= x_relay_0 * square;
-	minus 	  <= three - xy_square;
-	top       <= y_relay_3 * minus;
-     	y_next 	  <= shift_right(top, 1); -- divide by 2
+	    minus 	  <= three - xy_square;
+	    top       <= y_relay_3 * minus;
+      y_next 	  <= shift_right(top, 1); -- divide by 2
     	out_n 	<= y_next((4*F_bits -1) + (W_bits - F_bits) downto 4*F_bits -  F_bits);
 
     end if;
   end process;
 
   process (clock, reset)
-   begin 	
+  begin
     if(reset = '0') then
 	--out_n <= "0";
     elsif(rising_edge(clock)) then
-    	y_relay_0 <= y_in;
-	y_relay_1 <= y_relay_0;
-	y_relay_2 <= y_relay_1;
-	y_relay_3 <= y_relay_2;
-	x_relay_0 <= x;
+      y_relay_0 <= y_in;
+	    y_relay_1 <= y_relay_0;
+	    y_relay_2 <= y_relay_1;
+	    y_relay_3 <= y_relay_2;
+	    x_relay_0 <= x;
 
     end if;
   end process;
