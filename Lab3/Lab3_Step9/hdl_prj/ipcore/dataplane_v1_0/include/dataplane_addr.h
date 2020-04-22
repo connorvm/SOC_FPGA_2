2d3e63f86fdc8d79e1eb7d8a24bbdb1585f134f2
Connor <vanmeter.connor21@gmail.com>
2020-04-22 16:08:39 -0600

    Lab3_Step9 Material - including generated VHDL code

---
+++ Lab3/Lab3_Step9/hdl_prj/ipcore/dataplane_v1_0/include/dataplane_addr.h
@@ -0,0 +1,14 @@
+/*
+ * File Name:         hdl_prj\ipcore\dataplane_v1_0\include\dataplane_addr.h
+ * Description:       C Header File
+ * Created:           2020-04-22 15:45:06
+*/
+
+#ifndef DATAPLANE_H_
+#define DATAPLANE_H_
+
+#define  IPCore_Reset_dataplane       0x0  //write 0x1 to bit 0 to reset IP core
+#define  IPCore_Enable_dataplane      0x4  //enabled (by default) when bit 0 is 0x1
+#define  IPCore_Timestamp_dataplane   0x8  //contains unique IP timestamp (yymmddHHMM): 2004221545
+
+#endif /* DATAPLANE_H_ */
