71685b41ce4475e429df1a0cfe919caa7bfac956
Connor <vanmeter.connor21@gmail.com>
2020-04-24 11:17:41 -0600

    Added Lab3_Step10 material. Sound Effect not implemented yet.

---
+++ Lab3/Lab3_Step10/callback_init.m
@@ -0,0 +1,19 @@
+[input_wav_signal,Fs_in] = audioread('acoustic.wav');
+
+%% Set Aduio Data Sample Rate
+mp.Fs = 48000;   % sample rate of 48 kHz
+mp.Ts = 1/mp.Fs; % sample period
+
+%% Put signal into a timeseries format
+timevals = [0:length(signal)-1]*mp.Ts;
+input_signal = timeseries(signal,timevals);
+
+%% Set the data type for audio signal (left and right channels) indata plane
+mp.W_bits = 32;  % Word length
+mp.F_bits = 28; % Number of fractional bits in word
+
+%% Create test signals for the left and right channels
+stop_time = length(signal) * mp.Ts;
+
+%% Set the echo delay
+delay = 3;  % delay in seconds
