85683c10b0ce1dee50305f954e6246216fee6d1e
Alex Salois <asalois.scholar@gmail.com>
2020-05-07 10:51:03 -0600

    step 12 has generate hdl and now it is done

--- Lab3/Lab3_Step_12/hdl_prj/ipcore/dataplane_ip_v1_0/include/dataplane_ip_addr.h
+++ Lab3/Lab3_Step_12/hdl_prj/ipcore/dataplane_ip_v1_0/include/dataplane_ip_addr.h
@@ -1,7 +1,7 @@
 /*
  * File Name:         hdl_prj\ipcore\dataplane_ip_v1_0\include\dataplane_ip_addr.h
  * Description:       C Header File
- * Created:           2020-04-22 15:35:37
+ * Created:           2020-05-07 10:46:08
 */
 
 #ifndef DATAPLANE_IP_H_
@@ -9,6 +9,6 @@
 
 #define  IPCore_Reset_dataplane_ip       0x0  //write 0x1 to bit 0 to reset IP core
 #define  IPCore_Enable_dataplane_ip      0x4  //enabled (by default) when bit 0 is 0x1
-#define  IPCore_Timestamp_dataplane_ip   0x8  //contains unique IP timestamp (yymmddHHMM): 2004221535
+#define  IPCore_Timestamp_dataplane_ip   0x8  //contains unique IP timestamp (yymmddHHMM): 2005071046
 
 #endif /* DATAPLANE_IP_H_ */
--- Lab3/Lab3_Step_12/hdl_prj/ipcore/dataplane_v1_0/include/dataplane_addr.h
+++ Lab3/Lab3_Step_12/hdl_prj/ipcore/dataplane_v1_0/include/dataplane_addr.h
@@ -1,7 +1,7 @@
 /*
  * File Name:         hdl_prj\ipcore\dataplane_v1_0\include\dataplane_addr.h
  * Description:       C Header File
- * Created:           2020-04-22 15:45:06
+ * Created:           2020-05-07 10:47:51
 */
 
 #ifndef DATAPLANE_H_
@@ -9,6 +9,6 @@
 
 #define  IPCore_Reset_dataplane       0x0  //write 0x1 to bit 0 to reset IP core
 #define  IPCore_Enable_dataplane      0x4  //enabled (by default) when bit 0 is 0x1
-#define  IPCore_Timestamp_dataplane   0x8  //contains unique IP timestamp (yymmddHHMM): 2004221545
+#define  IPCore_Timestamp_dataplane   0x8  //contains unique IP timestamp (yymmddHHMM): 2005071047
 
 #endif /* DATAPLANE_H_ */
