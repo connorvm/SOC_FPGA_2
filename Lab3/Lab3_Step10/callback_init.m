ee6f7c83ee195f8ef99d8ff0a5eaf078902cab4a
Alex Salois <asalois.scholar@gmail.com>
2020-05-06 19:25:00 -0600

    fixed odd init function problem

--- Lab3/Lab3_Step10/callback_init.m
+++ Lab3/Lab3_Step10/callback_init.m
@@ -5,15 +5,15 @@ mp.Fs = 48000;   % sample rate of 48 kHz
 mp.Ts = 1/mp.Fs; % sample period
 
 %% Put signal into a timeseries format
-timevals = [0:length(signal)-1]*mp.Ts;
-input_signal = timeseries(signal,timevals);
+timevals = [0:length(input_wav_signal)-1]*mp.Ts;
+input_signal = timeseries(input_wav_signal,timevals);
 
 %% Set the data type for audio signal (left and right channels) indata plane
 mp.W_bits = 32;  % Word length
 mp.F_bits = 28; % Number of fractional bits in word
 
 %% Create test signals for the left and right channels
-stop_time = length(signal) * mp.Ts;
+stop_time = length(input_wav_signal) * mp.Ts;
 
 %% Set the echo delay
 delay = 3;  % delay in seconds
