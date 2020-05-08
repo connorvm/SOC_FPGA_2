405bbde41eaaa49ab681b4b40af3cf742a1bff88
Alexander Salois <asalois.scholar@gmail.com>
2020-05-07 22:54:34 -0600

    full quartus compile for step 13

--- Lab3/Lab3_Step13/quartus_linux/de10nano_projects/AudioMini_Passthrough/db/ip/sld6377880d/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Lab3/Lab3_Step13/quartus_linux/de10nano_projects/AudioMini_Passthrough/db/ip/sld6377880d/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : c457fe02012246d6ac7e
+//   DESIGN_HASH   : 9153eddd27852c15a525
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hc457fe02012246d6ac7e };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h9153eddd27852c15a525 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
